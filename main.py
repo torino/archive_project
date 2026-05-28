5611e08f7e6b509a31f1e8e9537459e0c0ef5298
torino <free.green.place@gmail.com>
2026-05-29 08:25:58 +0900

    エラー改修（関数修正未済箇所の反映）

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from capture import run_capture
 from build_ai_text import build_ai_texts
 from process import run_process
 from r2_upload import upload_today
-from generate_html import generate_html_all
 
 print("スクショ処理開始")
 
@@ -10,6 +9,5 @@ run_capture()
 run_process()
 build_ai_texts()
 upload_today()
-#generate_html_all()
 
 print("スクショ処理終了")
